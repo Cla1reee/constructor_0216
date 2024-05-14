22eeae67e401d114116862aa0edf3a6d124b569c
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2024-05-14 15:58:06 +0700

    membuat prosedur isidata

--- Destructor.cpp
+++ Destructor.cpp
@@ -25,6 +25,20 @@ angka::~angka(){
     cout << "Alamat Array Sudah Dilepaskan" << endl;
 }
 
+void angka::cetakData(){
+    for(int i=1;i<=panjang;i++){
+        cout << i << " = " << arr[i] << endl;
+    }
+}
+
+void angka::isiData(){
+    for (int i=1;i<=panjang;i++){
+        cout << i << " = ";
+        cin >> arr[1];
+    }
+    cout << endl;
+    
+}
 
 
 int main(){
